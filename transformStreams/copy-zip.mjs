4e27abf98212816ce704e736e3b7726eb736b27b
Rafael Noll <rafanoll2004@gmail.com>
2024-05-30 12:07:58 -0300

    Transform Stream: Making a coping script thats create a zip of file copied

---
+++ transformStreams/copy-zip.mjs
@@ -0,0 +1,42 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
+const [,,src, dest] = process.argv;
+
+const sourceStream = createReadStream(src);
+const destinationStream = createWriteStream(dest);
+const gzipStream = createGzip();
+
+sourceStream.on('data', (chunk) => {
+    const canContinue = gzipStream.write(chunk);
+
+    if(!canContinue){
+        sourceStream.pause();
+    }
+
+    gzipStream.once('drain', () => sourceStream.resume());
+}); 
+
+gzipStream.on('data', (chunk) => {
+
+    const canContinue = destinationStream.write(chunk);
+
+    if(!canContinue){
+        gzipStream.pause();
+    }
+
+    destinationStream.once('drain', () => gzipStream.resume());
+});
+
+sourceStream.on('end', () => {
+
+    const remaingData = gzipStream.read();
+
+    if(remaingData !== null){
+        destinationStream.write(remaingData);
+    }
+
+    gzipStream.end();
+});
+
+gzipStream.on('end', () => destinationStream.end());
