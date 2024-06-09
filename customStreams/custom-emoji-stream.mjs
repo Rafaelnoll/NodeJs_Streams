9f28f44ec66664a0ce82f1fdcd7d720a5786bf4e
Rafael Noll <rafanoll2004@gmail.com>
2024-06-09 11:28:48 -0300

    Custom Stream: Emoji stream

---
+++ customStreams/custom-emoji-stream.mjs
@@ -0,0 +1,32 @@
+// emoji-stream.js
+
+import emoji from 'emoji'
+import { Readable } from 'readable-stream'
+
+const emojis = Object.keys(emoji.EMOJI_MAP)
+
+function getEmojiDescription (index) {
+  return emoji.EMOJI_MAP[emojis[index]][1]
+}
+
+function getMessage (index) {
+  return emojis[index] + ' ' + getEmojiDescription(index)
+}
+
+export default class EmojiStream extends Readable {
+  constructor (options) {
+    super(options)
+    this._index = 0
+  }
+
+  _read () {
+    if (this._index >= emojis.length) {
+      return this.push(null)
+    }
+    return this.push(getMessage(this._index++))
+  }
+}
+
+const emojiStream = new EmojiStream();
+
+emojiStream.pipe(process.stdout);
